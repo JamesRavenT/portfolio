f20c270f3c7dac095f04bd8f222096ae46660937
JamesRavenT <jraven.tabag@gmail.com>
2024-06-02 07:11:46 +0800

    Added the missing slash

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "JRT-MyPortfolio.Web",
+  base: "/JRT-MyPortfolio.Web/",
 })
